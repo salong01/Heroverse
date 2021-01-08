8ed11ca28420b4590a41c000031a31cafd516f2f
salong01 <salong01@estudiantes.unileon.es>
2021-01-08 13:39:48 +0100

    updated angular core

--- heroverse/src/app/hero-details/hero-details.component.spec.ts
+++ heroverse/src/app/hero-details/hero-details.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { HeroDetailsComponent } from './hero-details.component';
 
@@ -6,7 +6,7 @@ describe('HeroDetailsComponent', () => {
   let component: HeroDetailsComponent;
   let fixture: ComponentFixture<HeroDetailsComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HeroDetailsComponent ]
     })
